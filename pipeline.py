b9d03d8df7b709caaba4e601437852db439ec67f
Foundry (vector-client-token) <>
2023-06-02 16:54:50 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,2 @@
 
 
-@transform_pandas(
-    Output(rid="ri.vector.main.execute.24cfa164-05c4-402f-91ba-dc80a5dbbc46"),
-    predictions_by_date=Input(rid="ri.foundry.main.dataset.647f3798-efd2-45ed-9a54-303cfb2c997e")
-)
-def unnamed(predictions_by_date):
-    
-
