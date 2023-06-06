f82b44b27d718b00ada9020b628e00f55bbc0a55
Foundry (vector-client-token) <>
2023-06-06 20:50:33 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,2 +1,13 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.9743acd5-fa1d-4d7b-8630-a34431e0777b")
+)
+analysis_1_COVID_positive_control_matching <- function(analysis_1_COVID_positive_control_pre_matching) {
+    library(MatchIt)
+    # add seed 
+    df <- analysis_1_COVID_positive_control_pre_matching
+    matching <- matchit(long_covid ~ data_partner_id + age_at_covid + observation_period_post_covid + index_date_numberofdays_from_20200101, data = df, method = 'nearest', exact = 'data_partner_id', caliper=c(age_at_covid = 10, index_date_numberofdays_from_20200101 = 45, observation_period_post_covid = 60), std.caliper = c(FALSE, FALSE, FALSE), ratio = 1)
+    return (match.data(matching))
+}
+
