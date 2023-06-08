a7d2e80e8b57fd6d828360298c594bf0913a87c6
Foundry (vector-client-token) <>
2023-06-08 15:55:18 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -3,20 +3,20 @@ from pyspark.sql.functions import when
 import numpy as np
 import pandas as pd
 import random
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import (roc_curve, roc_auc_score, confusion_matrix, accuracy_score, f1_score, precision_recall_curve, recall_score)
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import plot_roc_curve
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier
-from xgboost import plot_importance
-import matplotlib.pyplot as plt
-import math
+#import seaborn as sns
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import (roc_curve, roc_auc_score, confusion_matrix, accuracy_score, f1_score, precision_recall_curve, recall_score)
+#from sklearn.model_selection import RepeatedStratifiedKFold
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.metrics import roc_curve, auc
+#from sklearn.metrics import plot_roc_curve
+#from sklearn.metrics import roc_auc_score
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import accuracy_score
+#from xgboost import XGBClassifier
+#from xgboost import plot_importance
+#import matplotlib.pyplot as plt
+#import math
 
 
 #from lifelines import KaplanMeierFitter 
