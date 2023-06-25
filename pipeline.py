2529b98984e8f427f8d0240a65db32265b15487a
Foundry (vector-client-token) <>
2023-06-25 15:17:49 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,9 @@
 from pyspark.sql import functions as F
 from pyspark.sql.functions import when
 import numpy as np
-import pandas as pd
-import random
-import seaborn as sns
+#import pandas as pd
+#import random
+#import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (roc_curve, roc_auc_score, confusion_matrix, accuracy_score, f1_score, precision_recall_curve, recall_score)
 from sklearn.model_selection import RepeatedStratifiedKFold
@@ -13,13 +13,13 @@ from sklearn.metrics import plot_roc_curve
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier
-from xgboost import plot_importance
-import matplotlib.pyplot as plt
-import math
+#from xgboost import XGBClassifier
+#from xgboost import plot_importance
+#import matplotlib.pyplot as plt
+#import math
 
 
-from lifelines import KaplanMeierFitter 
+#from lifelines import KaplanMeierFitter 
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.f77735ea-fa94-412c-9b5d-82c314be0418"),
