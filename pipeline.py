a1a6196902156510e2a715c0d852fc87a83fdbf4
Foundry (vector-client-token) <>
2023-07-10 20:39:35 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1715,6 +1715,7 @@ def analysis_2b_xgboost(analysis_2b):
     
     y = df['COVID_patient_death_indicator']
     X = df.drop(columns = ['COVID_patient_death_indicator'])
+    features = list(X.columns)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
     classifier = XGBClassifier(random_state = 42)
     classifier.fit(X_train, y_train)
@@ -1761,6 +1762,13 @@ def analysis_2b_xgboost(analysis_2b):
     #fig.savefig("roc.png")
     plt.subplots_adjust(top=.8)
     plt.show()
+
+    #features_pd = pd.DataFrame (features, columns = ['feature'])
+    feature_importances = pd.DataFrame(classifier.feature_importances_, index = features, columns = ['importance'])
+    #features_pd = features_pd.join(feature_importances, 'feature', 'inner')
+    feature_importances['index_column'] = feature_importances.index
+    return feature_importances
+
     
 
 @transform_pandas(
