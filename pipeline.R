4abaa77f68251cdccc683da5170acfc885ca4202
Foundry (vector-client-token) <>
2023-07-11 19:47:15 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -225,3 +225,38 @@ df_clean_function <- function(df){
 
 }
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.ed814114-7489-40b6-bf5b-647053bf2d41"),
+    analysis_2b_logistic=Input(rid="ri.foundry.main.dataset.2a95bda9-68c8-4669-ae3f-977376ed6e98")
+)
+plot_odds_ratio_analysis_2b_1 <- function(analysis_2b_logistic) {
+    library(tidyverse)
+    library(ggplot2)
+    df <- analysis_2b_logistic
+    df <- df_clean_function(df)
+    
+    par(mfrow = c(1, 1))
+
+    # Creates color scheme for p value significance
+    SigCols <- c('Non-Significant'="skyblue2", 'Significant'="orange2")
+    
+    forest_plot1 = ggplot(data=df, aes(y=term, x=estimate, xmin=conf_low, xmax=conf_high, color=Pval_Signif)) + geom_point(size=3) + geom_errorbarh(size=0.85,height=.3) + scale_color_manual(values=SigCols) + labs(title='Outcome: patient death', x='Odds Ratio (OR)', y = 'Variable', color='p-Value Significance') + geom_vline(xintercept=1, color='black', linetype='dashed', alpha=.5) + theme_classic()+theme(text = element_text(size = 15)) + theme(axis.text.x= element_text(size=10))+theme(axis.text.y = element_text(size=10))
+    plot(forest_plot1)
+    
+
+    
+    return(df)
+    
+}
+
+df_clean_function <- function(df){
+
+    df <- df %>%
+        select(-statistic, -std_error) %>%
+        mutate('Pval_Signif'=ifelse(p_value<0.05, "Significant", "Non-Significant"))
+        
+    
+    return(df)
+
+}
+
