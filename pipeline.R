62a307cd21a91c536c2a24d57d124958848bade3
Foundry (vector-client-token) <>
2023-07-24 14:53:47 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -165,7 +165,7 @@ coxph <- function(coxph_prepare) {
     library("survminer")
 
     df <- coxph_prepare
-    res.cox <- coxph(Surv(duration, death)~PASC)
+    res.cox <- coxph(Surv(duration, death), data = df)
     print(summary(res.cox))
 
     #ggsurvplot(survfit(res.cox), conf.int = TRUE, legend.labs=c("PASC=0", "PASC=1"),
