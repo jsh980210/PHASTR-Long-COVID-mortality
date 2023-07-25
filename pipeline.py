3b3041cc452f6c8fc1294a2eaa51dd58fd06d1d7
Foundry (vector-client-token) <>
2023-07-25 16:01:56 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1699,10 +1699,10 @@ def analysis_2a_xgboost(analysis_2a):
     f1 = pd.Series({t: f1_score(y_true=y_test, y_pred=y_score>t) for t in ts})
     best_threshold = f1.idxmax()
     print(best_threshold)
-    plt.close()
-    plt.rcParams.update({'font.size': 10})
+    #plt.close()
+    
     fig, axes = plt.subplots(ncols=3, figsize=(13, 5))
-
+    plt.rcParams.update({'font.size': 10})
     sns.scatterplot(x='False Positive Rate', y='True Positive Rate', data=roc, s=50, legend=False, ax=axes[0])
     axes[0].plot('False Positive Rate', 'True Positive Rate', data=roc, lw=1, color='k')
     axes[0].plot(np.linspace(0,1,100), np.linspace(0,1,100), color='k', ls='--', lw=1)
