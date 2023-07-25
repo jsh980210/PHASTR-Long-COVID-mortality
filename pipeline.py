b0e71036ab35c23f9b2a99bd7312e80e590fd377
Foundry (vector-client-token) <>
2023-07-25 15:59:33 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1635,6 +1635,7 @@ def analysis_2a_logistic_py(analysis_2a):
     print("best_recall_threshold: ", best_threshold_recall)
     print("best_f1_threshold: ", best_threshold_f1)
     plt.close()
+    plt.rcParams.update({'font.size': 10})
     fig, axes = plt.subplots(ncols=4, figsize=(13, 5))
 
     sns.scatterplot(x='False Positive Rate', y='True Positive Rate', data=roc, s=50, legend=False, ax=axes[0])
