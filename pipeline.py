7899b5e81c10d91f3422bdac90b160eea086bae8
Foundry (vector-client-token) <>
2023-07-25 16:04:41 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1764,10 +1764,10 @@ def analysis_2a_xgboost_cv(analysis_2a):
 
     # 100 evenly spaced points from 0 to 1
     mean_fpr = np.linspace(0, 1, 100)
-    plt.close()
-    plt.rcParams.update({'font.size': 10})
+    
+    
     fig, ax = plt.subplots()
-
+    plt.rcParams.update({'font.size': 10})
     # Plot the individual ROC curves from the split
     for i, (train, test) in enumerate(cv.split(X, y)):
         classifier.fit(X[train], y[train])
