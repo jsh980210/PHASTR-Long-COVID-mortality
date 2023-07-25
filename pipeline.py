99afe13e7dc1af37a37a2e3a85b8a1edfbfd2cc8
Foundry (vector-client-token) <>
2023-07-25 16:42:34 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -2453,7 +2453,7 @@ def simplified_shap_plot_analysis_1(analysis_1_cohort):
     #return ABS_SHAP(shap_values,X_test)
     #plt.close()
     #plt.figure(figsize = (20, 6))
-    #plt.rcParams.update({'font.size': 10})
+    plt.rcParams.update({'font.size': 20})
     shap.summary_plot(shap_values, X_test, plot_size=[40, 15])
     plt.tight_layout()
     plt.show()
