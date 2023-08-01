4ec3935a37a2a965a76f6f7ae5663c458439089e
Foundry (vector-client-token) <>
2023-08-01 15:42:10 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1548,7 +1548,7 @@ def analysis_2a_logistic_cv(analysis_2a):
     mean_fpr = np.linspace(0, 1, 100)
     
     fig, ax = plt.subplots()
-    plt.rcParams.update({'font.size': 10})
+    #plt.rcParams.update({'font.size': 10})
     # Plot the individual ROC curves from the split
     for i, (train, test) in enumerate(cv.split(X, y)):
         classifier.fit(X[train], y[train])
@@ -1600,7 +1600,7 @@ def analysis_2a_logistic_cv(analysis_2a):
 
     set_output_image_type('svg')
     plt.rcParams['svg.fonttype'] = 'none'
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.show()
 
     return features_pd
